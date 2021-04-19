8c0a5e6bd22e1c2b3420c37196f080c81b871f9d
browonkim <browonkim@gmail.com>
2021-04-20 04:39:09 +0900

    add testfile

---
+++ mytest.c
@@ -0,0 +1,21 @@
+int a; char* str;
+float f[10];
+
+void main(int argc, char* argv) {
+    char c;      int d;
+    int i=0;
+    while( i < 10 ) {
+        a += (d + 1);
+        f[i++] = 0;
+    }
+return;
+}
+
+int myFunction(char* str) {
+    char c;
+    if( c!='c') {
+        str = strdup("test");
+        printf("test");
+    }
+return 0;
+}
